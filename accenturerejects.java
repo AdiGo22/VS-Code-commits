7fc4ebb96f1a29ec9360116cae9f4371911e61fd
coderadigo <coderadigo@gmail.com>
2023-09-08 00:49:29 +0530

    commit

--- accenturerejects.java
+++ accenturerejects.java
@@ -1,3 +1,5 @@
+/*input - string line accepting numbers a , b ,n with spaces 
+to find , number of steps required to monotically increase the sum when raised a to k times b or b  to k times a */
 import java.util.*;
 public class accenturerejects {
     public static int solve(int a , int b , int n){
@@ -18,6 +20,6 @@ public class accenturerejects {
         int b = Integer.valueOf(s[1]);
         int n = Integer.valueOf(s[2]);
         System.out.println(solve(a,b,n));
-
+        sc.close();
     }
 }
